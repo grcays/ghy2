599a3b8e90a4ea0e1907fe59deb0ee9a98fa2575
GHY <2350989985@qq.com>
2023-01-15 13:31:06 +0800

    hot-fix Test commit from GHY!

--- main/java/com/atguigu/git/GitTest.java
+++ main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,Git");
         System.out.println("second Hello,Git!");
         System.out.println("hot-fix third Hello,Git!");
+        System.out.println("hot-fix _4 演示合并冲突 Hello,Git!");
     }
 }
