b63a5106b841477fff02b932876558b8166eb15e
GHY <2350989985@qq.com>
2023-01-15 13:21:55 +0800

    hot fix first commit from GHY!

--- main/java/com/atguigu/git/GitTest.java
+++ main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git");
         System.out.println("second Hello,Git!");
+        System.out.println("hot-fix third Hello,Git!");
     }
 }
